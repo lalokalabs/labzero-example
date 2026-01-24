2ec0eff5c5162658435a94d17e66b05515a37590
Kamal Mustafa <kamal@koditi.my>
2026-01-24 12:53:19 +0000

    fix format

--- scripts/setup_dev.py
+++ scripts/setup_dev.py
@@ -8,6 +8,7 @@ django.setup()
 
 from django.urls import reverse
 from django.contrib.auth import get_user_model
+
 User = get_user_model()
-if not User.objects.filter(email='admin@myapp.co').exists():
-    User.objects.create_superuser(email='admin@myapp.co', password='picard data')
+if not User.objects.filter(email="admin@myapp.co").exists():
+    User.objects.create_superuser(email="admin@myapp.co", password="picard data")
